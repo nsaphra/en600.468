5a02749494762afca3211ff6bc5101f80a9e6946
N. Saphra <nsaphra@gmail.com>
2014-04-21 10:04:39 -0400

    still broken

--- reranker/mert.py
+++ reranker/mert.py
@@ -5,21 +5,21 @@ from bisect import bisect_left
 import copy
 
 grad = .01
-min_w = -1.0
+MIN_W = -1.0
 eps = .01
 
 class piecewise_fcn:
   def __init__(self, num_sent):
-    self.x = [min_w]
+    self.x = [MIN_W, -MIN_W]
     y_dflt = [-1 for sent in range(num_sent)]
-    self.y = [y_dflt]
+    self.y = [y_dflt, copy.copy(y_dflt)]
 
   def find_x(self, x, sent_ind):
     ind = bisect_left(self.x, x)
-    if ind < len(self.x)-1 and self.x[ind] == x:
+    if ind < len(self.x) and self.x[ind] == x:
       self.y[ind][sent_ind] = -1
       return ind
-    if ind == 0 or ind > len(self.x):
+    if ind == 0 or ind >= len(self.x):
       print "AGH WHAT"
       return -1
     self.x.insert(ind, x)
@@ -37,7 +37,7 @@ class piecewise_fcn:
 
   def find_max(self):
     bleu_scores = [0 for x in self.x]
-    for (ind, x) in enumerate(self.x):
+    for (ind, x) in enumerate(self.x[:-1]):
       bleu_stats = [0 for i in range(10)]
       for s in self.y[ind]:
         bleu_stats = [sum(scores) for scores in zip(s, bleu_stats)]
@@ -57,12 +57,12 @@ def sep_feat(feats, feat_name, weights):
 
 def upper_envelope(kbest_h, kbest_feats, bleu_stats, feat_name, weights):
   data = sorted([(sep_feat(feats, feat_name, weights), h_ind, bleu_) for (h_ind, (feats, h, bleu_)) in enumerate(zip(kbest_feats, kbest_h, bleu_stats))], key=lambda ((prev_slope, prev_const), prev_h_ind, prev_bleu): prev_slope)
-  (prev_score, prev_sort_ind) = max([(slope*min_w+const, sort_ind) for (sort_ind, ((slope, const), h_ind, bleu_)) in enumerate(data)], key=lambda (x,y):x)
+  (prev_score, prev_sort_ind) = max([(slope*MIN_W+const, sort_ind) for (sort_ind, ((slope, const), h_ind, bleu_)) in enumerate(data)], key=lambda (x,y):x)
   ((prev_slope, prev_const), prev_h_ind, prev_bleu) = data[prev_sort_ind]
   ret = []
-  prev_w_min = min_w
+  prev_w_min = MIN_W
   while (prev_sort_ind < len(kbest_h)):
-    (top_sort_ind, top_slope, top_const, top_w_min, top_score) = (-1, 0, 0, -min_w, -10000)
+    (top_sort_ind, top_slope, top_const, top_w_min, top_score) = (-1, 0, 0, -MIN_W, -10000)
     for (sort_ind_add, ((slope, const), h_ind, curr_bleu)) in enumerate(data[prev_sort_ind:]):
       sort_ind = sort_ind_add+prev_sort_ind
       if slope == prev_slope:
@@ -75,42 +75,13 @@ def upper_envelope(kbest_h, kbest_feats, bleu_stats, feat_name, weights):
         top_score = curr_score
         (top_sort_ind, top_slope, top_const, top_w_min, top_score) = (sort_ind, slope, const, curr_w_min, curr_score)
 
-    if top_sort_ind < 0 or top_w_min >= -min_w:
-      ret.append((prev_h_ind, prev_w_min, -min_w, prev_bleu))
+    if top_sort_ind < 0 or top_w_min >= -MIN_W:
+      ret.append((prev_h_ind, prev_w_min, -MIN_W, prev_bleu))
       break
     ret.append((prev_h_ind, prev_w_min, top_w_min, prev_bleu))
     prev_sort_ind = top_sort_ind
     prev_w_min = top_w_min
     ((prev_slope, prev_const), prev_h_ind, prev_bleu) = data[top_sort_ind]
-
-    # assert(curr_w_min > prev_w_min)
-    top_h_ind = data[top_sort_ind][1]
-    # score = 0.0
-    # for (k, feat) in kbest_feats[top_h_ind].items():
-    #   if k == feat_name:
-    #     score += feat*top_w_min
-    #   else:
-    #     score += feat * weights[k]
-    # score__ = sum([w * kbest_feats[top_h_ind][name] for (name, w) in weights.items()]) - kbest_feats[top_h_ind][feat_name] * weights[feat_name] + kbest_feats[top_h_ind][feat_name] * top_w_min
-    # (s_, c_) = sep_feat(kbest_feats[top_h_ind], feat_name, weights)
-    # print "====="
-    # print "%f == %f == %f == %f" % (top_score, score__, score, c_+top_w_min*kbest_feats[top_h_ind][feat_name])
-    # print top_score - score__
-    # print top_score - score
-    # assert(top_score == score__)
-    # assert(top_score == score)
-    # for (i, feats) in enumerate(kbest_feats):
-    #   score= sum([w * kbest_feats[i][name] for (name, w) in weights.items()]) - kbest_feats[i][feat_name] * weights[feat_name] + kbest_feats[i][feat_name] * top_w_min
-    #   if i == top_h_ind:
-    #     assert(top_score == score)
-    #   else:
-    #     if top_score < score:
-    #       print "%f %f %f" % (slope, const, slope*top_w_min+const)
-    #       print top_score - score
-    #       print "%d %d" % (i, top_h_ind)
-    #       print "%f < %f" % (top_score, score)
-        #assert(top_score >= score)
-
   return ret
 
 def mert(weights, data):
@@ -122,8 +93,9 @@ def mert(weights, data):
     for (sent_ind, d) in data.items():
       for (h_ind, w_min, w_max, curr_bleu_stats) in upper_envelope(d['kbest'], d['kbest_feats'], d['bleu'], name, weights):
         f.incr(w_min, w_max, sent_ind, curr_bleu_stats)
-    for b in f.y:
+    for (i,b) in enumerate(f.y):
       if -1 in b:
+        print f.x[i]
         print b
     (top_w, b) = f.find_max()
     new_weights[name] = top_w
