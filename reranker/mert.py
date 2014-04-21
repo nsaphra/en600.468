709a6ae3ae40a28a86b65e3459825f16211ad672
N. Saphra <nsaphra@gmail.com>
2014-04-21 16:24:37 -0400

    wordcnt brevity penalty

--- reranker/mert.py
+++ reranker/mert.py
@@ -4,9 +4,10 @@ import bleu
 from bisect import bisect_left
 import copy
 import random
+from math import exp
 
 grad = .05
-MIN_W = -2.0
+MIN_W = -5.0
 eps = .05
 
 class piecewise_fcn:
@@ -21,7 +22,7 @@ class piecewise_fcn:
       self.y[ind][sent_ind] = -1
       return ind
     if ind == 0 or ind >= len(self.x):
-      print "AGH WHAT"
+      sys.stderr.write( "AGH WHAT")
       return -1
     self.x.insert(ind, x)
     self.y.insert(ind, copy.copy(self.y[ind-1]))
@@ -95,29 +96,39 @@ def mert(weights, data):
     new_weights[name] = top_w
   return new_weights
 
-def get_feats(h, feats):
+def get_feats(h, s, feats):
   f = {}
   for feat in feats.split(' '):
     (k, v) = feat.split('=')
     f[k] = float(v)
+  f['word_cnt'] = exp((1-len(s))/len(h)) if len(h) <= len(s) else 1
+  # f['untranslated_cnt'] = 0.0
+  # for htok in h:
+  #   if htok in s:
+  #     f['untranslated_cnt'] += 1.0
   return f
 
-def performance(weights, dev_src, dev_kbest, dev_ref):
+def performance(weights, dev_src, dev_kbest, dev_ref, print_out=False):
   all_hyps = [pair.split(' ||| ') for pair in open(dev_kbest)]
   num_sents = len(all_hyps) / 100
   stats = [0 for i in xrange(10)]
   ref_file = open(dev_ref)
-  for (ref, s) in zip(ref_file, xrange(0, num_sents)):
+  src_file = open(dev_src)
+  for (ref, src, s) in zip(ref_file, src_file, xrange(0, num_sents)):
     hyps_for_one_sent = all_hyps[s * 100:s * 100 + 100]
     ref = ref.strip().split()
+    src = src.strip().split()
     (best_score, best) = (-1e300, '')
-    for (num, hyp, feats) in hyps_for_one_sent:
+    for (num, h_sent, feats) in hyps_for_one_sent:
       score = 0.0
-      for (k,v) in get_feats(hyp, feats).items():
+      hyp = h_sent.strip().split()
+      for (k,v) in get_feats(hyp, src, feats).items():
         score += weights[k] * v
       if score > best_score:
-        (best_score, best) = (score, hyp)
+        (best_score, best) = (score, h_sent)
     stats = [sum(scores) for scores in zip(stats, bleu.bleu_stats(best.strip().split(),ref))]
+    if print_out:
+      print best
   return bleu.bleu(stats)
 
 def main():
@@ -143,45 +154,38 @@ def main():
     ind = sent_ind_from_id[sent_id]
     s = data[ind]['src']
     h = h_sent.strip().split()
-    feats = {}
-    for feat in base_feats.split(' '):
-      (k, v) = feat.split('=')
-      feats[k] = float(v)
-    # feats['word_cnt'] = len(h)
-    # feats['untranslated_cnt'] = 0
-    # for htok in h:
-    #   if htok in s:
-    #     feats['untranslated_cnt'] += 1
+    feats = get_feats(h, s, base_feats)
     data[ind]['kbest'].append(h)
     data[ind]['kbest_feats'].append(feats)
 
-  # bleu_stats = [0 for i in range(10)]
-  # for (ind, d) in data.items():
-  #   bleu_stats = [sum(scores) for scores in zip(bleu_stats, bleu.bleu_stats(d['ref'], d['ref']))]
   for (ind, d) in data.items():
     r = d['ref']
     for (h, feats) in zip(d['kbest'], d['kbest_feats']):
       d['bleu'].append([score for score in bleu.bleu_stats(h,r)])
 
-  weights = {'p(e)' : opts.lm, 'p(e|f)' : opts.tm1, 'p_lex(f|e)' : opts.tm2} #, 'word_cnt':-1.0, 'untranslated_cnt':-1.0}
-  print "iter -1"
-  print "train BLEU %f" % performance(weights, opts.src, opts.kbest, opts.ref)
-  print "test BLEU %f" % performance(weights, "data/dev+test.src", "data/dev+test.100best", "data/dev.ref")
+  shortcuts = {'p(e)' : 'l', 'p(e|f)' : 't', 'p_lex(f|e)' : 'u', 'word_cnt' : 'c'}
+  weights = {'p(e)' : opts.lm, 'p(e|f)' : opts.tm1, 'p_lex(f|e)' : opts.tm2, 'word_cnt':-0.5}#, 'untranslated_cnt':-1.0}
+  sys.stderr.write( "iter -1\n")
+  sys.stderr.write( "train BLEU %f\n" % performance(weights, opts.src, opts.kbest, opts.ref))
+  sys.stderr.write( "test BLEU %f\n" % performance(weights, "data/dev+test.src", "data/dev+test.100best", "data/dev.ref"))
+  out = ""
+  for (n, w) in weights.items():
+    out += "-%s %s " % (shortcuts[n], w)
+  sys.stderr.write( out + "\n")
   best_bleu = 0.0
   best_test = 0.0
   best_w = weights
-  shortcuts = {'p(e)' : 'l', 'p(e|f)' : 't', 'p_lex(f|e)' : 'u'}
   for i in range(10):
     new_weights = mert(weights, data)
-    print "iter %d" % i
+    sys.stderr.write( "iter %d\n" % i)
     train_bleu = performance(new_weights, opts.src, opts.kbest, opts.ref)
     test_bleu = performance(new_weights, "data/dev+test.src", "data/dev+test.100best", "data/dev.ref")
-    print "train BLEU %f" % train_bleu
-    print "test BLEU %f" % test_bleu
+    sys.stderr.write( "train BLEU %f\n" % train_bleu)
+    sys.stderr.write( "test BLEU %f\n" % test_bleu)
     out = ""
     for (n, w) in new_weights.items():
       out += "-%s %s " % (shortcuts[n], w)
-    print out
+    sys.stderr.write( out + "\n")
     if train_bleu > best_bleu:
       best_bleu = train_bleu
       best_test = test_bleu
@@ -191,18 +195,16 @@ def main():
       diff += abs(w - new_weights[n])
     weights = new_weights
     if diff <= eps:
-      print "RANDOM RESTART"
+      sys.stderr.write( "RANDOM RESTART\n")
       for name in weights.keys():
         weights[name] = random.uniform(MIN_W, 0.0)
-  print "BEST:"
-  print "train BLEU %f" % best_bleu
-  print "test BLEU %f" % best_test
+  sys.stderr.write( "BEST:\n")
+  sys.stderr.write( "train BLEU %f\n" % best_bleu)
+  sys.stderr.write( "test BLEU %f\n" % performance(best_w, "data/dev+test.src", "data/dev+test.100best", "data/dev.ref", print_out=True))
   out = ""
   for (n, w) in best_w.items():
     out += "-%s %s " % (shortcuts[n], w)
-  print out
-
-
+  sys.stderr.write( out + "\n")
 
 if __name__ == '__main__':
   main()
